590b3d62f73aa87a6f30fe0a37e11b777da0913b
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-06-22 13:58:37 -0700

    implement the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div className='app'>
+      <Search />
+      <div className='container'>
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60ºF</h1>
+          </div>
+          <div className="description">
+            <p>cloud</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65ºF</p>
+          </div>
+          <div className="humidity">
+            <p>20º</p>
+          </div>
+          <div className="wind">
+            12MPH
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='search'>
+      <input type="search" placeholder='Enter Location'/>
+    </div>
+  )
+}
+
+export default Search
